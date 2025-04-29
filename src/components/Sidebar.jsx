a6992dbe633888c2a315c5990b7008fb87fc2d40
Natthawat Pomsuwan <natthawat.pm@rmuti.ac.th>
2025-04-29 20:04:24 +0700

    add holiday manage page

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -44,6 +44,7 @@ const adminNav = [
   { to: "/admin/organization-manage", text: "จัดการองค์กร", icon: <FaUsersCog /> },
   { to: "/admin/department-manage", text: "จัดการแผนก", icon: <FaUsersCog /> },
   { to: "/admin/personel-manage", text: "จัดการประเภทบุคคล", icon: <FaUsersCog /> },
+  { to: "/admin/holiday-manage", text: "จัดการวันหยุด", icon: <FaUsersCog /> },
   { to: "/admin/edit-profile", text: "การตั้งค่า", icon: <FaUsersCog /> },
 ];
 
