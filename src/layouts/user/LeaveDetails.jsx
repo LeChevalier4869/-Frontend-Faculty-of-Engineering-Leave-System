a2f432c3a81af1ddb9917ad16eaba5e3f8ca52af
Natthawat Pomsuwan <natthawat.pm@rmuti.ac.th>
2025-05-01 19:42:53 +0700

    fix leave detail page not compleate

--- src/layouts/user/LeaveDetails.jsx
+++ src/layouts/user/LeaveDetails.jsx
@@ -73,27 +73,205 @@ export default function LeaveDetail() {
   return (
     <div className="min-h-screen bg-white px-6 py-10 font-kanit text-black">
       <div className="max-w-5xl mx-auto bg-gray-50 p-6 rounded-xl shadow">
-        <div className="flex items-center gap-3 mb-6">
+        <div className="flex items-center justify-center gap-3 mb-6">
           <FaFileAlt className="text-gray-700 text-3xl" />
           <h1 className="text-3xl font-bold">รายละเอียดใบลา</h1>
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          <Item
-            label="ชื่อ-นามสกุล"
-            value={`${user?.prefixName}${user?.firstName} ${user?.lastName}`}
-          />
-          <Item label="ประเภทการลา" value={leaveType?.name || "-"} />
-          <Item
-            label="ช่วงวันลา"
-            value={`ตั้งแต่ ${formatDate(startDate)} ถึง ${formatDate(
-              endDate
-            )}`}
-          />
-          <Item label="จำนวนวันที่ลา" value={`${totalDays || 0} วัน`} />
-          <Item label="เหตุผล/หมายเหตุ" value={reason || "-"} />
-          <Item label="ที่อยู่ระหว่างลา" value={contact || "-"} />
-          <Item label="เบอร์ติดต่อ" value={contactPhone || "-"} />
+          <div className="md:col-span-2 flex justify-end">
+            <div className="flex items-center gap-2">
+              <label className="text-sm font-medium text-gray-700">
+                เลขที่ใบลา:
+              </label>
+              <p className="bg-white border border-gray-200 px-4 py-2 rounded-lg text-gray-800">
+                12345678
+              </p>
+            </div>
+          </div>
+          <div className="md:col-span-2 flex justify-end">
+            <div className="flex items-center gap-2">
+              <label className="text-sm font-medium text-gray-700">
+                วัน/เดือน/ปี:
+              </label>
+              <p className="bg-white border border-gray-200 px-4 py-2 rounded-lg text-gray-800">
+                1 พฤษภาคม 2567
+              </p>
+            </div>
+          </div>
+          <div className="md:col-span-2 flex justify-start">
+            <div className="flex items-center gap-2">
+              <label className="text-sm font-medium text-gray-700">
+                เรื่อง:
+              </label>
+              <p className="bg-white border border-gray-200 px-4 py-2 rounded-lg text-gray-800">
+                ขอ{leaveType?.name || "-"}
+              </p>
+            </div>
+          </div>
+          <div className="md:col-span-2 flex justify-start">
+            <div className="flex items-center gap-2">
+              <label className="text-sm font-medium text-gray-700">
+                เรียน:
+              </label>
+              <p className="bg-white border border-gray-200 px-4 py-2 rounded-lg text-gray-800">
+                คณบดี/ผู้อำนวยการสำนักงานวิทยาเขตขอนแก่น
+              </p>
+            </div>
+          </div>
+          <div className="md:col-span-2 pl-8">
+            <div className="flex items-center gap-8 w-full">
+              <div className="flex items-center gap-2 w-1/2">
+                <label className="text-sm font-medium text-gray-700 whitespace-nowrap">
+                  ข้าพเจ้า
+                </label>
+                <p className="bg-white border border-gray-200 px-4 py-2 rounded-lg text-gray-800 w-full">
+                  {`${user?.prefixName}${user?.firstName} ${user?.lastName}`}
+                </p>
+              </div>
+              <div className="flex items-center gap-2 w-1/2">
+                <label className="text-sm font-medium text-gray-700 whitespace-nowrap">
+                  ตำแหน่ง
+                </label>
+                <p className="bg-white border border-gray-200 px-4 py-2 rounded-lg text-gray-800 w-full">
+                  {user.position || "-"}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="md:col-span-2 pl-8">
+            <div className="flex items-center gap-8 w-full">
+              <div className="flex items-center gap-2 w-1/2">
+                <label className="text-sm font-medium text-gray-700 whitespace-nowrap">
+                  สังกัด
+                </label>
+                <p className="bg-white border border-gray-200 px-4 py-2 rounded-lg text-gray-800 w-full">
+                  {`${user.department?.organization?.name || "-"}`}
+                </p>
+              </div>
+              <div className="flex items-center gap-2 w-1/2">
+                <label className="text-sm font-medium text-gray-700 whitespace-nowrap">
+                  ประเภท
+                </label>
+                <p className="bg-white border border-gray-200 px-4 py-2 rounded-lg text-gray-800 w-full">
+                  {user.personnelType?.name || "-"}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="md:col-span-2 pl-8">
+            <div className="flex items-center gap-8 w-full">
+              <div className="flex items-center gap-2 w-1/2">
+                <label className="text-sm font-medium text-gray-700 whitespace-nowrap">
+                  ขออนุญาต
+                </label>
+                <p className="bg-white border border-gray-200 px-4 py-2 rounded-lg text-gray-800 w-full">
+                  {`${leaveType?.name || "-"}`}
+                </p>
+              </div>
+              <div className="flex items-center gap-2 w-1/2">
+                <label className="text-sm font-medium text-gray-700 whitespace-nowrap">
+                  เนื่องจาก
+                </label>
+                <p className="bg-white border border-gray-200 px-4 py-2 rounded-lg text-gray-800 w-full">
+                  {reason || "-"}
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="md:col-span-2">
+            <div className="flex items-center gap-8 w-full">
+              <div className="flex items-center gap-2 w-1/2">
+                <label className="text-sm font-medium text-gray-700 whitespace-nowrap">
+                  ตั้งแต่
+                </label>
+                <p className="bg-white border border-gray-200 px-4 py-2 rounded-lg text-gray-800 w-full">
+                  {formatDate(startDate)}
+                </p>
+              </div>
+              <div className="flex items-center gap-2 w-1/2">
+                <label className="text-sm font-medium text-gray-700 whitespace-nowrap">
+                  ถึง
+                </label>
+                <p className="bg-white border border-gray-200 px-4 py-2 rounded-lg text-gray-800 w-full">
+                  {formatDate(endDate)}
+                </p>
+              </div>
+              <div className="flex items-center gap-2 w-1/6">
+                <label className="text-sm font-medium text-gray-700 whitespace-nowrap">
+                  มีกำหนด
+                </label>
+                <p className="bg-white border border-gray-200 px-4 py-2 rounded-lg text-gray-800 text-center w-full">
+                  {totalDays}
+                </p>
+                <label className="text-sm font-medium text-gray-700 whitespace-nowrap">
+                  วัน
+                </label>
+              </div>
+            </div>
+          </div>
+
+          <div className="md:col-span-2">
+            <div className="flex items-center gap-8 w-full">
+              <div className="flex items-center gap-2 flex-[1.2]">
+                <label className="text-sm font-medium text-gray-700 whitespace-nowrap">
+                  ข้าพเจ้าได้
+                </label>
+                <p className="bg-white border border-gray-200 px-4 py-2 rounded-lg text-gray-800 text-center w-full">
+                  {leaveType?.name}
+                </p>
+              </div>
+              <div className="flex items-center gap-2 flex-[2]">
+                <label className="text-sm font-medium text-gray-700 whitespace-nowrap">
+                  ครั้งสุดท้ายเมื่อ
+                </label>
+                <p className="bg-white border border-gray-200 px-4 py-2 rounded-lg text-gray-800 w-full">
+                  {formatDate(startDate)}
+                </p>
+              </div>
+              <div className="flex items-center gap-2 flex-[2]">
+                <label className="text-sm font-medium text-gray-700 whitespace-nowrap">
+                  ถึง
+                </label>
+                <p className="bg-white border border-gray-200 px-4 py-2 rounded-lg text-gray-800 w-full">
+                  {formatDate(endDate)}
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="md:col-span-2">
+            <div className="flex items-center gap-4 w-full">
+              <div className="flex items-center gap-2 flex-[0.8]">
+                <label className="text-sm font-medium text-gray-700 whitespace-nowrap">
+                  มีกำหนด
+                </label>
+                <p className="bg-white border border-gray-200 px-4 py-2 rounded-lg text-gray-800 text-center w-full">
+                  {totalDays}
+                </p>
+                <label className="text-sm font-medium text-gray-700 whitespace-nowrap">
+                  วัน
+                </label>
+              </div>
+              <div className="flex items-center gap-2 flex-[3.5]">
+                <label className="text-sm font-medium text-gray-700 whitespace-nowrap">
+                  ช่องทางติดต่อ
+                </label>
+                <p className="bg-white border border-gray-200 px-4 py-2 rounded-lg text-gray-800 w-full">
+                  {contact || "-"}
+                </p>
+              </div>
+              <div className="flex items-center gap-2 flex-[1.7]">
+                <label className="text-sm font-medium text-gray-700 whitespace-nowrap">
+                  หมายเลขโทรศัพท์
+                </label>
+                <p className="bg-white border border-gray-200 px-4 py-2 rounded-lg text-gray-800 w-full">
+                  {user.phone || "-"}
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
 
         <div className="mt-8">
@@ -183,9 +361,10 @@ const Item = ({ label, value }) => (
 
 const formatDate = (dateStr) => {
   if (!dateStr) return "-";
-  return new Date(dateStr).toLocaleDateString("th-TH", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
+  const date = new Date(dateStr);
+  const day = date.getDate();
+  const month = date.toLocaleDateString("th-TH", { month: "long" });
+  const year = date.getFullYear() + 543;
+
+  return `วันที่ ${day} ${month} พ.ศ. ${year}`;
 };
