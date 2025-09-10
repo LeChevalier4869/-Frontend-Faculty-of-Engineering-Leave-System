4fbb63006cd32abbc26c5619e856db05c9d6dcc7
Natthawat Pomsuwan <natthawat.pm@rmuti.ac.th>
2025-09-10 18:39:46 +0700

    debug auth contex

--- src/layouts/oauth/Callback.jsx
+++ src/layouts/oauth/Callback.jsx
@@ -3,37 +3,26 @@ import { useNavigate } from "react-router-dom";
 
 export default function Callback() {
   const navigate = useNavigate();
+  const { user ,setUser } = useAuth();
 
-  useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const accessToken = urlParams.get("access");
-    const refreshToken = urlParams.get("refresh");
+  const token = res.data.token;
+  localStorage.setItem("token", token);
 
-    if (accessToken && refreshToken) {
-      // เก็บ token ลง localStorage
-      localStorage.setItem("accessToken", accessToken);
-      localStorage.setItem("refreshToken", refreshToken);
-
-      console.log("Access:", accessToken);
-      console.log("Refresh:", refreshToken);
-
-      // redirect ไป dashboard
-      navigate("/dashboard");
-    } else {
-      // ถ้าไม่มี token → กลับไป login
-      navigate("/login");
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(apiEndpoints.getMe, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      const user = res.data;
+      setUser(user);
+    } catch (err) {
+      console.error("fetchUser error:", err);
     }
-  }, [navigate]);
+  };
+  fetchUser();
 
-  return <p>กำลังเข้าสู่ระบบ...</p>;
-}
-
-
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-
-export default function Callback() {
-  const navigate = useNavigate();
+  // ตรวจสอบ roles ในรูปแบบที่ถูกต้อง
+  // const roles = (user?.roles || []).map((role) => role.roleName);
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
