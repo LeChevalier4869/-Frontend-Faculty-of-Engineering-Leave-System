7672ac1188db0b98287d690143b109868818ddc1
Natthawat Pomsuwan <natthawat.pm@rmuti.ac.th>
2025-10-06 04:51:50 +0700

    calendar page filter year

--- src/layouts/admin/HolidayManage.jsx
+++ src/layouts/admin/HolidayManage.jsx
@@ -180,7 +180,7 @@ export default function HolidayManage() {
         <h1 className="text-3xl font-bold mb-8 text-center">จัดการวันหยุด</h1>
 
         {/* Form */}
-        <div className="mb-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 ">
+        <div className="mb-6 flex flex-col sm:flex-row items-center justify-around gap-4">
           <input
             type="date"
             value={date}
