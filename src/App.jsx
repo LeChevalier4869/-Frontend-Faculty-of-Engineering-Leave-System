9d196892c833b33accac6ee1729fd290e3f7574b
Natthawat Pomsuwan <natthawat.pm@rmuti.ac.th>
2025-10-07 05:00:19 +0700

    reduce splash screen to 1 time after login)

--- src/App.jsx
+++ src/App.jsx
@@ -2,51 +2,44 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import useAuth from "./hooks/useAuth";
 import AppRouter from "./routes/AppRouter";
-import logo from "./assets/logo.png";
+import logo from "./assets/logo.png"; // โลโก้ใน src/assets/
 
 function App() {
   const { loading } = useAuth();
-
-  // แสดง Splash เฉพาะตอนไม่มี token
-  const [showSplash, setShowSplash] = useState(() => {
-    const token = localStorage.getItem("accessToken");
-    return !token; // true = ยังไม่มี token → แสดง Splash
-  });
+  const [showSplash, setShowSplash] = useState(true);
   const [fadeOut, setFadeOut] = useState(false);
 
   useEffect(() => {
-    if (!loading && showSplash) {
+    if (!loading) {
+      // เริ่ม fade-out หลัง splash แสดง 2 วินาที
       const timer1 = setTimeout(() => setFadeOut(true), 1500);
+      // ซ่อน splash 1 วินาทีหลังเริ่ม fade-out
       const timer2 = setTimeout(() => setShowSplash(false), 3000);
 
       return () => {
-        clearTimeout(timer1);
+        clearTimeout(timer1)
         clearTimeout(timer2);
       };
     }
-  }, [loading, showSplash]);
+  }, [loading]);
 
-  return (
-    <>
-      {/* AppRouter จะอยู่ตลอด */}
-      <AppRouter />
+  if (showSplash) {
+    return (
+      <div
+        className={`flex flex-col items-center justify-center h-screen bg-gray-900 text-white font-kanit
+        ${fadeOut ? "animate-fade-out" : "animate-fade-in"}`}
+      >
+        <img
+          src={logo}
+          alt="Logo"
+          className="w-24 h-24 mb-6 animate-bounce"
+        />
+        <p className="text-3xl font-semibold">กำลังโหลดระบบ...</p>
+      </div>
+    );
+  }
 
-      {/* Splash overlay */}
-      {showSplash && (
-        <div
-          className={`fixed inset-0 z-50 flex flex-col items-center justify-center bg-gray-900 text-white font-kanit
-            ${fadeOut ? "animate-fade-out" : "animate-fade-in"}`}
-        >
-          <img
-            src={logo}
-            alt="Logo"
-            className="w-24 h-24 mb-6 animate-bounce"
-          />
-          <p className="text-3xl font-semibold">กำลังโหลดระบบ...</p>
-        </div>
-      )}
-    </>
-  );
+  return <AppRouter />;
 }
 
 export default App;
