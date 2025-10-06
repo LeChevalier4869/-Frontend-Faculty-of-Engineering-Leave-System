2692289e8712c6af0e552019757efe92a7f86eca
Natthawat Pomsuwan <natthawat.pm@rmuti.ac.th>
2025-10-07 04:30:41 +0700

    reduce splash screen to 1 time after login)

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./contexts/AuthContext.jsx";
 import ReactDOM from "react-dom/client";
 import React from "react";
-import './index.css'
+import "./index.css";
 import App from "./App.jsx";
 import { LeaveRequestContextProvider } from "./contexts/LeaveRequestContext.jsx";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthContextProvider>
-      <LeaveRequestContextProvider>
-        <App />
-      </LeaveRequestContextProvider>
-    </AuthContextProvider>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <LeaveRequestContextProvider>
+          <App />
+        </LeaveRequestContextProvider>
+      </AuthContextProvider>
+    </BrowserRouter>
   </StrictMode>
 );
-
