c8127a9b1fedc44acc82de7f6fef12f9bc7d7288
Natthawat Pomsuwan <natthawat.pm@rmuti.ac.th>
2025-10-07 04:55:17 +0700

    reduce splash screen to 1 time after login)

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,26 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import useAuth from "./hooks/useAuth";
 import AppRouter from "./routes/AppRouter";
-import logo from "./assets/logo.png";
+import logo from "./assets/logo.png"; // โลโก้ใน src/assets/
 
 function App() {
   const { loading } = useAuth();
-  
-  // ✅ เริ่มต้น: ถ้ามี token → ไม่ต้องโชว์ Splash
-  const [showSplash, setShowSplash] = useState(() => {
-    const token = localStorage.getItem("accessToken");
-    return !token; // true = ยังไม่มี token → แสดง Splash
-  });
+  const [showSplash, setShowSplash] = useState(true);
   const [fadeOut, setFadeOut] = useState(false);
 
   useEffect(() => {
-    if (!loading && showSplash) {
-      // ✅ ตั้ง timer fade out
+    if (!loading) {
+      // เริ่ม fade-out หลัง splash แสดง 2 วินาที
       const timer1 = setTimeout(() => setFadeOut(true), 1500);
+      // ซ่อน splash 1 วินาทีหลังเริ่ม fade-out
       const timer2 = setTimeout(() => setShowSplash(false), 3000);
 
       return () => {
-        clearTimeout(timer1);
+        clearTimeout(timer1)
         clearTimeout(timer2);
       };
     }
-  }, [loading, showSplash]);
+  }, [loading]);
 
   if (showSplash) {
     return (
@@ -33,7 +29,11 @@ function App() {
         className={`flex flex-col items-center justify-center h-screen bg-gray-900 text-white font-kanit
         ${fadeOut ? "animate-fade-out" : "animate-fade-in"}`}
       >
-        <img src={logo} alt="Logo" className="w-24 h-24 mb-6 animate-bounce" />
+        <img
+          src={logo}
+          alt="Logo"
+          className="w-24 h-24 mb-6 animate-bounce"
+        />
         <p className="text-3xl font-semibold">กำลังโหลดระบบ...</p>
       </div>
     );
