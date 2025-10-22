25c1678b5a89115602729bc8aa9762bde4b9893a
DIS-Trainee <134470483+DIS-Trainee@users.noreply.github.com>
2025-10-22 23:23:51 +0700

    init

--- src/layouts/admin/AddOtherRequest.jsx
+++ src/layouts/admin/AddOtherRequest.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import apiEndpoints from "../../utils/api";
+import { apiEndpoints } from "../../utils/api";
 
 function AddOtherRequest() {
 
